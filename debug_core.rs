066aee912c9f933e0e703fd5d26c94881e7b8aea
Ralph Loader <ralph1loader@gmail.com>
2025-12-04 13:15:28 +1300

    First shared files

---
+++ debug_core.rs
@@ -0,0 +1,165 @@
+//! UART debug output.  This drives a STM32 serial port.  This is designed to
+//! be used another crate directly, rather than being contained in our own
+//! crate.
+//!
+//! We assume that the crate we are part of contains a few things...
+
+use crate::cpu::{WFE, barrier};
+use crate::vcell::{UCell, VCell};
+
+use super::{DEBUG, NODEBUG, UART, UART_ISR, lazy_init};
+
+use core::fmt::Write;
+
+
+pub struct DebugMarker;
+
+pub struct Debug {
+    pub w: VCell<u8>,
+    pub r: VCell<u8>,
+    buf: [UCell<u8>; 256],
+}
+
+pub fn debug_isr() {
+    if !NODEBUG {
+        DEBUG.isr();
+    }
+}
+
+impl const Default for Debug {
+    fn default() -> Debug {
+        Debug {
+            w: VCell::new(0), r: VCell::new(0),
+            buf: [const {UCell::new(0)}; 256]
+        }
+    }
+}
+
+impl Debug {
+    pub fn write_bytes(&self, s: &[u8]) {
+        if NODEBUG {
+            return;
+        }
+        lazy_init();
+        let mut w = self.w.read();
+        for &b in s {
+            while self.r.read().wrapping_sub(w) == 1 {
+                self.enable(w);
+                self.push();
+            }
+            // SAFETY: The ISR won't access the array element in question.
+            unsafe {*self.buf[w as usize].as_mut() = b};
+            w = w.wrapping_add(1);
+        }
+        self.enable(w);
+    }
+    fn push(&self) {
+        WFE();
+        // If the interrupt is pending, call the ISR ourselves.  Read the bit
+        // twice in case there is a race condition where we read pending on an
+        // enabled interrupt.
+        let nvic = unsafe {&*cortex_m::peripheral::NVIC::PTR};
+        let bit: u32 = 1 << UART_ISR as u32;
+        if nvic.icpr[0].read() & bit == 0 {
+            return;
+        }
+        // It might take a couple of goes for the pending state to clear, so
+        // loop.
+        while nvic.icpr[0].read() & bit != 0 {
+            unsafe {nvic.icpr[0].write(bit)};
+            debug_isr();
+        }
+    }
+    fn enable(&self, w: u8) {
+        barrier();
+        self.w.write(w);
+
+        let uart = unsafe {&*UART::ptr()};
+        // Use the FIFO empty interrupt.  Normally we should be fast enough
+        // to refill before the last byte finishes.
+        uart.CR1.write(
+            |w| w.FIFOEN().set_bit().TE().set_bit().UE().set_bit()
+                . TXFEIE().set_bit());
+    }
+    fn isr(&self) {
+        let uart = unsafe {&*UART::ptr()};
+        let sr = uart.ISR.read();
+        if sr.TC().bit() {
+            uart.CR1.modify(|_,w| w.TCIE().clear_bit());
+        }
+        if !sr.TXFE().bit() {
+            return;
+        }
+
+        const FIFO_SIZE: usize = 8;
+        let mut r = self.r.read() as usize;
+        let w = self.w.read() as usize;
+        let mut done = 0;
+        while r != w && done < FIFO_SIZE {
+            uart.TDR.write(|w| w.bits(*self.buf[r].as_ref() as u32));
+            r = (r + 1) & 0xff;
+            done += 1;
+        }
+        self.r.write(r as u8);
+        if r == w {
+            uart.CR1.modify(|_,w| w.TXFEIE().clear_bit());
+        }
+    }
+}
+
+pub fn flush() {
+    if !super::is_init() {
+        return;                        // Not initialized, nothing to do.
+    }
+
+    let uart = unsafe {&*UART::ptr()};
+    // Enable the TC interrupt.
+    uart.CR1.modify(|_,w| w.TCIE().set_bit());
+    // Wait for the TC bit.
+    loop {
+        let isr = uart.ISR.read();
+        if DEBUG.r.read() == DEBUG.w.read()
+            && isr.TC().bit() && isr.TXFE().bit() {
+            break;
+        }
+        DEBUG.push();
+    }
+}
+
+pub fn write_str(s: &str) {
+    DEBUG.write_bytes(s.as_bytes());
+}
+
+impl Write for DebugMarker {
+    fn write_str(&mut self, s: &str) -> core::fmt::Result {
+        write_str(s);
+        Ok(())
+    }
+    fn write_char(&mut self, c: char) -> core::fmt::Result {
+        let cc = [c as u8];
+        DEBUG.write_bytes(&cc);
+        Ok(())
+    }
+}
+
+#[macro_export]
+macro_rules! dbg {
+    ($($tt:tt)*) => {
+        if !$crate::debug::NODEBUG {
+            let _ = core::fmt::Write::write_fmt(
+                &mut $crate::debug::debug_core::DebugMarker, format_args!($($tt)*));
+        }
+    }
+}
+
+#[macro_export]
+macro_rules! dbgln {
+    () => {if !$crate::debug::NODEBUG {
+        let _ = core::fmt::Write::write_str(
+            &mut $crate::debug::debug_core::DebugMarker, "\n");
+        }};
+    ($($tt:tt)*) => {if !$crate::debug::NODEBUG {
+        let _ = core::fmt::Write::write_fmt(
+            &mut $crate::debug::debug_core::DebugMarker, format_args_nl!($($tt)*));
+        }};
+}
---
+++ vcell.rs
@@ -0,0 +1,58 @@
+#![allow(dead_code)]
+
+use core::cell::UnsafeCell;
+
+/// Interrupt safe volatile cell, has read/write for scalar types.
+#[repr(transparent)]
+pub struct VCell<T>(UnsafeCell<T>);
+
+/// A basic cell for storage.  Shared access is safe, mutable access is
+/// unsafe.
+#[repr(transparent)]
+#[derive_const(Default)]
+pub struct UCell<T>(UnsafeCell<T>);
+
+unsafe impl<T: Sync> Sync for VCell<T> {}
+unsafe impl<T: Sync> Sync for UCell<T> {}
+
+impl<T: const Default> const Default for VCell<T> {
+    fn default() -> Self {Self::new(T::default())}
+}
+
+impl<T> VCell<T> {
+    pub const fn new(v: T) -> Self {Self(UnsafeCell::new(v))}
+    pub fn as_ptr(&self) -> *mut T {self.0.get()}
+    pub fn as_mut(&mut self) -> &mut T {self.0.get_mut()}
+}
+
+impl<T: Sync> UCell<T> {
+    pub const fn new(v: T) -> Self {Self(UnsafeCell::new(v))}
+    pub fn as_ref(&self) -> &T {unsafe{&*(self.0.get() as *const T)}}
+    pub fn as_ptr(&self) -> *mut T {self.0.get()}
+    /// Get mutable access.  SAFETY:  It is up to the caller to ensure that
+    /// mutability is handled correctly.
+    pub unsafe fn as_mut(&self) -> &mut T {unsafe{&mut *self.0.get()}}
+}
+
+impl<T: Sync> core::ops::Deref for UCell<T> {
+    type Target = T;
+    fn deref(&self) -> &T {self.as_ref()}
+}
+
+macro_rules! VCellImpl {
+    ($($t:ty),*) => {$(
+        impl VCell<$t> {
+            #[inline(always)]
+            pub fn read(&self) -> $t {
+                unsafe {core::ptr::read_volatile(self.as_ptr())}
+            }
+            #[inline(always)]
+            pub fn write(&self, v: $t) {
+                unsafe {core::ptr::write_volatile(self.as_ptr(), v)};
+            }
+        }
+    )*};
+}
+
+// VCellImpl!(bool, i8, u8, i16, u16, i32, u32, isize, usize, char);
+VCellImpl!(u8);
